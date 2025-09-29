70409f688de8fccb909fdd7a4d22daf2c201496d
Luan M Paes <luanmp25@gmail.com>
2025-09-29 10:05:06 -0300

    Atualizado

--- src/main/java/com/devsuperior/dsmeta/dto/SaleMinDTO.java
+++ src/main/java/com/devsuperior/dsmeta/dto/SaleMinDTO.java
@@ -4,33 +4,49 @@ import java.time.LocalDate;
 
 import com.devsuperior.dsmeta.entities.Sale;
 
+
 public class SaleMinDTO {
+		private Long id;
+	    private LocalDate date;
+	    private Double amount;
+	    private String sellerName;  // só no DTO
 
-	private Long id;
-	private Double amount;
-	private LocalDate date;
-	
-	public SaleMinDTO(Long id, Double amount, LocalDate date) {
-		this.id = id;
-		this.amount = amount;
-		this.date = date;
-	}
-	
-	public SaleMinDTO(Sale entity) {
-		id = entity.getId();
-		amount = entity.getAmount();
-		date = entity.getDate();
-	}
+	    public SaleMinDTO(Sale entity) {
+	        this.id = entity.getId();
+	        this.date = entity.getDate();
+	        this.amount = entity.getAmount();
+	        this.sellerName = entity.getSeller().getName(); // pega o nome do seller
+	    }
 
-	public Long getId() {
-		return id;
-	}
+		public Long getId() {
+			return id;
+		}
 
-	public Double getAmount() {
-		return amount;
-	}
+		public void setId(Long id) {
+			this.id = id;
+		}
 
-	public LocalDate getDate() {
-		return date;
+		public LocalDate getDate() {
+			return date;
+		}
+
+		public void setDate(LocalDate date) {
+			this.date = date;
+		}
+
+		public Double getAmount() {
+			return amount;
+		}
+
+		public void setAmount(Double amount) {
+			this.amount = amount;
+		}
+
+		public String getSellerName() {
+			return sellerName;
+		}
+
+		public void setSellerName(String sellerName) {
+			this.sellerName = sellerName;
+		}   
 	}
-}
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,11 +1,18 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
 
@@ -14,10 +21,41 @@ public class SaleService {
 
 	@Autowired
 	private SaleRepository repository;
+
+	private SaleSummaryDTO getSummary;
+
+	private SaleReportDTO getReport;
 	
 	public SaleMinDTO findById(Long id) {
-		Optional<Sale> result = repository.findById(id);
-		Sale entity = result.get();
+		Sale entity = repository.findById(id)
+	.orElseThrow(()-> new RuntimeException("Venda não encontrada!"));
 		return new SaleMinDTO(entity);
+	}
+
+	public List<SaleSummaryDTO>getSummary(String minDateStr, String maxDateStr){
+		LocalDate today = LocalDate.now();
+
+		LocalDate maxDate = (maxDateStr == null || maxDateStr.isEmpty())
+				? today : LocalDate.parse(maxDateStr, DateTimeFormatter.ISO_DATE);
+
+		LocalDate minDate = (minDateStr == null || minDateStr.isEmpty())
+				? maxDate.minusYears(1) : LocalDate.parse(minDateStr,DateTimeFormatter.ISO_DATE);
+
+		return repository.sumary(minDate, maxDate);
+	}
+
+	public Page<SaleReportDTO>getReport(String minDateStr, String maxDateStr, String name, Pageable pageable){
+
+		LocalDate today = LocalDate.now();
+		
+		LocalDate maxDate = (maxDateStr == null || maxDateStr.isEmpty())
+				? today : LocalDate.parse(maxDateStr, DateTimeFormatter.ISO_DATE);
+		
+		LocalDate minDate = (minDateStr == null || minDateStr.isEmpty())
+				? maxDate.minusYears(1) : LocalDate.parse(minDateStr, DateTimeFormatter.ISO_DATE);
+		
+		String nameFilter = (name == null) ? "" : name;
+		
+		return repository.report(minDate, maxDate, nameFilter, pageable);
 	}
 }
